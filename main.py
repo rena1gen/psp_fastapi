f6cba185440ebb3d6ff1b18d6b9a1c4d747d9b1c
rena1gen <renatgen2002@gmail.com>
2024-09-20 02:54:09 +0300

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from jinja2 import Environment, FileSystemLoader
 
 app = FastAPI()
 
+
+# Создаем Jinja2 Environment
+templates = Environment(loader=FileSystemLoader('templates'))
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    template = templates.get_template('index.html')
+    rendered_template = template.render()
+    return HTMLResponse(content=rendered_template)
 
 @app.get("/")
 async def root():
